2a804c7a91a31cf69df2d0a930b27a3a1d14001d
Sameer Mirza <srmirza14@gmail.com>
2023-05-23 19:24:18 -0400

    double checked scripts and HTML to ensure quiz works + added README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Javscript Quiz Game With Timer
+
+Welcome to the javascript quiz game ! This quiz was created using `HTML`, `CSS`, and `Javascript`.
+
+## How To Play
+- - - -
+
+## What happens when the timer runs out?
+- - - -
+
+## Save highscore functionality
+- - - -
+
+## Conclusion
+- - - -
+
+The quiz is live and available to play at:
+
+The answers for testing purposes are '1', 'A Disney Licensed RPG', 'A TV Show', and 'Washington D.C.' in no particular order.
--- assets/js/finScreen.js
+++ assets/js/finScreen.js
@@ -1,5 +1,45 @@
-// query selector const vars + localstorage
+// query selector vars + localstorage var
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const recentScore = localStorage.getItem('recentScore');
+
+// highscores var to parse our scores in local storage and return them as string
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+// var for how many max highscores we can have
+const maxHighScores = 5;
+
+// finalscores innertext will be set as our recentScore local storage strings
+finalScore.innerText = recentScore;
+
+// on keyup the username button will be re enabled
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+});
+
+// savehighscore function
+saveHighScore = e => {
+    e.preventDefault();
+
+    // this object array will store user score and inputted name
+    const score = {
+        score: recentScore,
+        name: username.value
+    };
+
+    // we then push the highscores into our score obj array
+    highScores.push(score);
+
+    // they are then sorted in a, b format aka username:score
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    });
+
+    // array is then spliced into 5 recent scores
+    highScores.splice(5);
+
+    // they are set in the localstorage again for later use
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('./');
+};
